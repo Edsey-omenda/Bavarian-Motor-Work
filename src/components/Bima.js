3e9c5ea1f80c5789a822cfe93dc4c5ff579990ee
Edsey omenda <omendajames0317@gmail.com>
2022-09-05 00:48:02 +0300

    creates  badges  for  giving  insight  about  bmw

--- src/components/Bima.js
+++ src/components/Bima.js
@@ -7,10 +7,12 @@ export default function Bavarian(){
  <div>
     <img src={pic} alt="bmw-icon" style={{width : 75 + 'rem'}} /> <br></br> <br></br>
  <div className='nav-link text-blue h4' to="/">
-    100% BMW CERTIFIED
+    100% BMW CERTIFIED   &nbsp; 
+    <span class="badge rounded-pill text-bg-primary">Learn more</span>
     <br></br>
     <br></br>
-    100% SUMMER CERTIFIED
+    100% SUMMER CERTIFIED   &nbsp; 
+    <span class="badge rounded-pill text-bg-info">Pre-Owned Inventory</span>
  </div>
  </div>
  
