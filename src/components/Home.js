5c597747aa97b5712c095cdef602eca87da87b0f
Edsey omenda <omendajames0317@gmail.com>
2022-09-05 02:17:50 +0300

    updates  the  component by  linking  it  with  the  new components to  incorporate  them in  the  app

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import Bavarian from './Bima'
+import Interior from './Interior'
+import Cosy     from  './Cosy'
+import  Parts    from  './Parts'
+import   Garage   from  './Garage'
+
 export default function Home(){
-
-
 
 
     return(
         <div className="text-bg-secondary p-3">
         <div className='container'>Home
-        <center>
+        {/* <center> */}
         <div className='row' >
           <h2>Welcome to Bavarian Motor Works</h2>
           <Bavarian />
+          {/* <div className="col m-2"> */}
+          <div className='card' style={{width : 25 + 'rem'}} > 
+          <Interior />
+          <Cosy  />
+          <Garage />
+          <Parts />
           </div>
-        </center>
+          </div>
+          {/* </div> */}
+        {/* </center> */}
         </div>
         </div>
     )
