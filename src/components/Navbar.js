823b3ad11d04c6ca3d6a56dfc500c11271397312
Edsey omenda <omendajames0317@gmail.com>
2022-09-04 22:46:56 +0300

    defines the home component and updates it to render a welcome message

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,7 @@ export default function Home(){
 
     return(
         <div className="text-bg-info p-3">
-        <div className='container'>Home
-        <center>
-        <div className='row' >
-          <h2>Welcome to Bavarian Motor Works</h2>
-          </div>
-        </center>
-        </div>
+       
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './components/Navbar';
+import Home from './components/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar />
+    <Home />
   </React.StrictMode>
 );
 
