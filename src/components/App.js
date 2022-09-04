e808138236adac91aa5fd3d3b9297c977a458d40
Edsey omenda <omendajames0317@gmail.com>
2022-09-04 23:59:49 +0300

    imports  the  models and reviews components  and  sets route for each  in the nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React  from 'react'
 import Navbar from  './Navbar'
 import Home from './Home'
+import Models from './Models';
+import Reviews from './Review';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 export default  function App(){
       return(
-        <div>
-      <Navbar />
-        </div>
+        <Router> 
+            <div>
+        <Navbar />
+          </div>
+
+          <Routes>
+          <Route  exact path="/" element={<Home />}></Route>
+          <Route  exact path="/models" element={<Models />}></Route>
+          <Route  exact path="/reviews" element={<Reviews />}></Route>
+          </Routes>
+          </Router>
+       
       )
 }
