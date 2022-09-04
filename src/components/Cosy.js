a553ec586d6ef753606d6dbbb4fa906671b895db
Edsey omenda <omendajames0317@gmail.com>
2022-09-05 02:18:37 +0300

    defines  the cosy  function  and  exports  it  to  home  component  for display

---
+++ src/components/Cosy.js
@@ -0,0 +1,13 @@
+import  React  from  'react'
+import pic from "../Assets/cosyx5.png";
+
+
+export default  function Cosy(){
+
+
+    return(
+        <div className="col m-2">
+            <img src={pic} alt="bmw-interior" style={{width : 25 + 'rem'}}/>
+        </div>
+    )
+}
