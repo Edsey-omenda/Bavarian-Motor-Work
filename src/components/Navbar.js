d72adc8a89afc04a6145935a73aa9ff22386da4a
Edsey omenda <omendajames0317@gmail.com>
2022-09-04 23:36:54 +0300

    imports  the  Navbar into  the  app  to  display  the  nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,31 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 
 export default function Home(){
 
     return(
         <div className="text-bg-info p-3">
-        <nav className="navbar navbar-dark bg-dark fixed-top">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="bmw">Offcanvas dark navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="bmwoffcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-            <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
-                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div className="offcanvas-body">
-                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="bmw">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="bmw">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="bmw" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <ul className="dropdown-menu dropdown-menu-dark">
-                    <li><a className="dropdown-item" href="bmw">Action</a></li>
-                    <li><a className="dropdown-item" href="bmw">Another action</a></li>
-                    <li>
-                        <hr className="dropdown-divider"></hr>
-                    </li>
-                    <li><a className="dropdown-item" href="bmw">Something else here</a></li>
-                    </ul>
-                </li>
-                </ul>
-                <form class="d-flex" role="search">
-                {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> */}
-                <button class="btn btn-success" type="submit">Search</button>
-                </form>
-            </div>
-            </div>
-        </div>
-        </nav>
+        <div className="collapse" id="navbarToggleExternalContent">
+  <div className="bg-dark p-4">
+  <ul className="nav nav-pills mb-3" id="pills-tabContent" role="tablist">
+        < li className="nav-item" role="presentation">
+        <Link className='nav-link ' to="/">Home</Link>
+        </li>
+ </ul>
+  <Link className='nav-link text-white h4' to="/">Home</Link>
+    <h5 className="text-white h4">Home</h5>
+    <h5 className="text-white h4">Models</h5>
+    <h5 className="text-white h4">Reviews</h5>
+  </div>
+</div>
+<nav className="navbar navbar-dark bg-dark">
+  <div className="container-fluid">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
         </div>
         )
 }
