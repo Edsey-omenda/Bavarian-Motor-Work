eb2ac3a8df2be33e159303910be714fab475236c
Edsey omenda <omendajames0317@gmail.com>
2022-09-04 23:38:26 +0300

    imports  browser routes  for  creating  routing  of various  components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React  from 'react'
 import Navbar from  './Navbar'
+import Home from './Home'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 export default  function App(){
       return(
