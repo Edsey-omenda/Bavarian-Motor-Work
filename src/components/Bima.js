ad956566bf1304c176c55732961c507712a2c5b9
Edsey omenda <omendajames0317@gmail.com>
2022-09-05 01:46:12 +0300

    updates Bima  component

--- src/components/Bima.js
+++ src/components/Bima.js
@@ -5,14 +5,17 @@ import pic from "../Assets/bmw.jpg";
 export default function Bavarian(){
  return (
  <div>
+
     <img src={pic} alt="bmw-icon" style={{width : 75 + 'rem'}} /> <br></br> <br></br>
  <div className='nav-link text-blue h4' to="/">
+    <p className='card-text'>
     100% BMW CERTIFIED   &nbsp; 
-    <span class="badge rounded-pill text-bg-primary">Learn more</span>
+    <a href ="https://www.bmwusa.com/vehicles/x-models/x1/sports-activity-vehicle/overview.html" span class="badge rounded-pill text-bg-primary">Learn more</a>
     <br></br>
     <br></br>
     100% SUMMER CERTIFIED   &nbsp; 
-    <span class="badge rounded-pill text-bg-info">Pre-Owned Inventory</span>
+    <a href ="#" span  class="badge rounded-pill text-bg-info" >Pre-Owned Inventory</a>
+    </p>
  </div>
  </div>
  
