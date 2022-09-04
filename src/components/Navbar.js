951d2339c34df0a909e49fcebb1a302a761d0952
Edsey omenda <omendajames0317@gmail.com>
2022-09-04 22:41:16 +0300

    First commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+export default function Home(){
+
+
+
+
+    return(
+        <div className="text-bg-info p-3">
+        <div className='container'>Home
+        <center>
+        <div className='row' >
+          <h2>Welcome to Bavarian Motor Works</h2>
+          </div>
+        </center>
+        </div>
+        </div>
+    )
+}
