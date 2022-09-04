e4ed4adca56e2283a110fe492bafb84c699c45e7
Edsey omenda <omendajames0317@gmail.com>
2022-09-05 02:21:57 +0300

    defines  the  interior  function for  displaying interior  component  information and  exports  it  to  the  home component

--- src/components/Interior.js
+++ src/components/Interior.js
@@ -1,11 +1,17 @@
 import  React  from  'react'
+import pic from "../Assets/interiorBMW.jpg";
 
 
 export default  function Interior(){
 
 
     return(
-        <div>
+        <div className="col m-2">
+            <img src={pic} alt="bmw-interior" style={{width : 25 + 'rem'}}/>
+            <div className='card-text'>
+            <h4>Is the BMW X5 a Good SUV?</h4>
+         </div>
         </div>
+       
     )
 }
