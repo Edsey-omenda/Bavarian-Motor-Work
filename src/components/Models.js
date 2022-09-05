1da76e87581d473e62d84fea9266d66ab2682c8e
Edsey omenda <omendajames0317@gmail.com>
2022-09-05 22:24:11 +0300

    imports  the useEffect  and  useState to fetch  data from the  db.json  and  passes  it  to  the  modelcard  for  visual  display

---
+++ src/components/Models.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import ModelCard from './ModelCard';
+
+
+export default function Models(){
+    const[models, setModels] = useState([])
+
+    useEffect(() =>{
+        fetch("http://localhost:4000/models")
+        .then(response => response.json())
+        .then((models) =>{
+        setModels(models)
+        // console.log(models)
+    })
+    })
+
+    let myModels = models.map((model) => (<ModelCard 
+        modelTitle={model.title} 
+        modelThumbnail={model.thumbnail} 
+        modelDescription={model.short_description} 
+        modelReleaseYear={model.release_date} 
+        id={model.id}
+        key={model.id} />))
+
+
+    return(
+        <div className="text-bg-info p-3">
+        <div className='container'>My Models
+          <div className='row'>
+            {myModels}
+          </div>
+        </div>
+        </div>
+    )
+}
