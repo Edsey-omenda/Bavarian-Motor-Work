ee6a0aaa68e3cd7dd76cf13530b78178d4c954b7
Edsey omenda <omendajames0317@gmail.com>
2022-09-05 16:48:51 +0300

    adds an introductory  head text  about  the  component

--- src/components/Cosy.js
+++ src/components/Cosy.js
@@ -1,5 +1,5 @@
 import  React  from  'react'
-import pic from "../Assets/cosyx5.png";
+import pic from "../Assets/2020-BMW-X5M-Competition-and-X6M-Competition-3.jpg";
 
 
 export default  function Cosy(){
@@ -7,7 +7,13 @@ export default  function Cosy(){
 
     return(
         <div className="col m-2">
-            <img src={pic} alt="bmw-interior" style={{width : 25 + 'rem'}}/>
+            <img src={pic} alt="bmw-interior" style={{width : 28 + 'rem', height : 24 + 'rem'}}/>
+            <div className='card-text'>
+            <h4>Always  moving  and  evolving</h4>
+            <p>A fresh  perspective of the  SUV X5 &nbsp;
+            <a href ="https://www.motortrend.com/cars/bmw/x5/2020/"  class="badge rounded-pill text-bg-info" >Learn more</a>
+            </p>
+         </div>
         </div>
     )
 }
