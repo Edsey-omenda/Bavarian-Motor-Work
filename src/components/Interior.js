9ffa9e016c24699be63ec9141f854ea806ee2c03
Edsey omenda <omendajames0317@gmail.com>
2022-09-05 16:53:22 +0300

    adds a badge with a link  to give more insight  about  car  interior  and parts

--- src/components/Interior.js
+++ src/components/Interior.js
@@ -7,9 +7,14 @@ export default  function Interior(){
 
     return(
         <div className="col m-2">
-            <img src={pic} alt="bmw-interior" style={{width : 25 + 'rem'}}/>
+            <img src={pic} alt="bmw-interior" style={{width : 25 + 'rem', height : 25 + 'rem'}}/>
             <div className='card-text'>
             <h4>Is the BMW X5 a Good SUV?</h4>
+            <p>The X5, BMW's midsize luxury SUV, 
+            entered its fourth generation for the 2019 model year 
+            and introduces an M Sport model for 2020. &nbsp;
+            <a href ="https://www.motortrend.com/cars/bmw/x5/2020/"  class="badge rounded-pill text-bg-info" >Learn more</a>
+            </p>
          </div>
         </div>
        
--- src/components/Parts.js
+++ src/components/Parts.js
@@ -1,5 +1,5 @@
 import  React  from  'react'
-import pic from "../Assets/bmwparts.jpg";
+import pic from "../Assets/2020-BMW-X6M-Competition-engine.jpg";
 
 
 export default  function Parts(){
@@ -7,7 +7,13 @@ export default  function Parts(){
 
     return(
         <div className="col m-2">
-            <img src={pic} alt="bmw-spare-part" style={{width : 25 + 'rem'}}/>
+            <img src={pic} alt="bmw-spare-part" style={{width : 28 + 'rem', height : 24 + 'rem'}}/>
+            <div className='card-text'>
+            <h4>The epitome  of  Bavarian Spirit</h4>
+            <p>Appreciate  the  light of movement  by  this  machine &nbsp;
+            <a href ="https://www.bmwofarlington.com/bmw-x5-specs-arlington-tx/"  class="badge rounded-pill text-bg-info" >Learn more</a>
+            </p>
+         </div>
         </div>
     )
 }
