13a7c6c51b89aa9b633f8ce61c607beea2d76fbc
Edsey omenda <omendajames0317@gmail.com>
2022-09-05 16:49:58 +0300

    updates  the  garage  component and  adds  a paragraph for  description

--- src/components/Garage.js
+++ src/components/Garage.js
@@ -7,7 +7,13 @@ export default  function Garage(){
 
     return(
         <div className="col m-2">
-            <img src={pic} alt="bmw-interior" style={{width : 25 + 'rem'}}/>
+            <img src={pic} alt="bmw-interior" style={{width : 25 + 'rem', height : 25 + 'rem'}}/>
+            <div className='card-text'>
+            <h4>The beating heart of  any  city!</h4>
+            <p>Machine  preying upon the unknowing &nbsp;
+            <a href ="https://www.bmwofarlington.com/bmw-x5-specs-arlington-tx/"  class="badge rounded-pill text-bg-info" >Learn more</a>
+            </p>
+         </div>
         </div>
     )
 }
