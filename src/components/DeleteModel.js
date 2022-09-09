c866c9b40b9029bc6f5a68f6ee47bcfb0e2c4fc3
Edsey omenda <omendajames0317@gmail.com>
2022-09-10 00:20:54 +0300

    creates a delete component  responsible  for  handling  the  delete button

---
+++ src/components/DeleteModel.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+export  default  function Delete(){
+
+    const[models, setModels] = useState([])
+
+ 
+    function handleDeleteModel(e){
+     let id = e.target.value
+     fetch(`http://localhost:9292/models/${id}`,{
+       method:"DELETE"
+     })
+     .then((res)=>res.json())
+     .then(()=>setModels([...models]))
+   }
+     
+
+    return(
+
+        <div>
+            <button className="remove" value={models.id} onClick={handleDeleteModel}> <span role="img" aria-label="delete">
+              🗑
+            </span>Delete</button>
+        </div>
+    )
+}
