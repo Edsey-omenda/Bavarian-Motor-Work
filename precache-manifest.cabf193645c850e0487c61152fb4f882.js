a2ee3f52a2c6c2437638d0f07770f891382f2e78
Edsey omenda <omendajames0317@gmail.com>
2022-09-10 15:55:14 +0300

    Updates

---
+++ precache-manifest.cabf193645c850e0487c61152fb4f882.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "de9802e4f532a39ddc0d",
+    "url": "/Bavarian-Motor-Work/static/js/main.de9802e4.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/Bavarian-Motor-Work/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "46e2f405ce4467b94c2d",
+    "url": "/Bavarian-Motor-Work/static/js/2.46e2f405.chunk.js"
+  },
+  {
+    "revision": "657679c5f065bb051577",
+    "url": "/Bavarian-Motor-Work/static/js/runtime~main.657679c5.js"
+  },
+  {
+    "revision": "62a6abe910f38d366113412d55abb5a6",
+    "url": "/Bavarian-Motor-Work/static/media/bmw.62a6abe9.jpg"
+  },
+  {
+    "revision": "a3a8e590a3def0affb8add79dd5d3fd6",
+    "url": "/Bavarian-Motor-Work/static/media/interiorBMW.a3a8e590.jpg"
+  },
+  {
+    "revision": "9fa27c058549044a107b80e5ca804dcf",
+    "url": "/Bavarian-Motor-Work/static/media/2020-BMW-X5M-Competition-and-X6M-Competition-3.9fa27c05.jpg"
+  },
+  {
+    "revision": "f1c2529f284d30a8aa9ea22ce4e73054",
+    "url": "/Bavarian-Motor-Work/static/media/2020-BMW-X6M-Competition-engine.f1c2529f.jpg"
+  },
+  {
+    "revision": "65c23e3cefaa64e863161873e84a2355",
+    "url": "/Bavarian-Motor-Work/static/media/garageBMW.65c23e3c.jpg"
+  },
+  {
+    "revision": "be42e1b54160bb1922ecc001563738aa",
+    "url": "/Bavarian-Motor-Work/index.html"
+  }
+];
